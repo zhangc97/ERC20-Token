4a8c8df3809a2b936b961a07a445e0a808df5baf
Chray Zhang <zhangc16@mcmaster.ca>
2018-05-13 15:12:41 -0400

    7 Passed tests for transfer

--- Test/contracts/Crowdsale.sol
+++ Test/contracts/Crowdsale.sol
@@ -1,64 +1,57 @@
 pragma solidity ^0.4.18;
 
-import "./ERCReceivingContract.sol";
 import "./Token.sol";
 import "./TokenFromScratch3.sol";
 import "./ERCReceivingContract.sol";
 import "./SafeMath.sol";
 
-contract CrowdSale is ERCReceivingContract { //need ERC223 to notify when the contract recieves Tokens
+contract Crowdsale { //need ERC223 to notify when the contract recieves Tokens
 
   using SafeMath for uint256;
 
-  Token private _token; //place the token comes from
-  uint private _deadline; //deadline
-  uint private _price; //price per token
-  uint private _limit; //cap
-  uint private _available; //amount left
-  uint private _raised; //raised amount
-  address private _wallet; //wallet im sending my coins to
+  TokenFromScratch3 public _token; //place the token comes from
+  uint256 private _price; //price per token
+  uint256 private _limit; //cap
+  uint256 private _available; //amount left
+  uint256 private _raised; //raised amount
+  address public _wallet; //wallet im sending my coins to
+  uint256 private _start;
+  uint256 private _end;
 
-  event Buy(address beneficiary , uint amount);
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
 
-  mapping(address => uint) private _limits;
+  mapping(address => uint) private _contributions;
 
-  modifier valid(address recipient, uint value) {
-    assert(value > 0);
-    uint amount = value/_price;
-    assert(_limit >= amount);
-    assert(_limits[recipient].add(amount) <= _limit );
-    _;
-  }
-
-  modifier available() {
-    require(_available >0);
-    require(now <= _deadline);
-    _;
-  }
-
-  modifier isOver() {
-    require(_deadline > now);
-    _;
-  }
-
-  modifier isToken() {
+/*-------------------Modifiers-------------------------*/
+  modifier isToken {
     require(msg.sender == address(_token));
     _;
   }
+  //modifier onlyWhileOpen {
+    // solium-disable-next-line security/no-block-members
+  //  require(block.timestamp >= _openingTime && block.timestamp <= _closingTime);
+  //  _;
+  //}
 
-
-
-  function CrowdSale(address token, uint deadline, uint price, uint limit, address wallet) public {
-      _token = Token(token); //setting address tokens are coming from
-      _deadline = deadline; //setting deadline time
+  function Crowdsale(uint256 price, address wallet, TokenFromScratch3 token) public {
+      require(price > 0);
+      require(wallet != address(0));
+      require(token != address(0));
+      _token = token; //setting address tokens are coming from
+      //_deadline = deadline; //setting deadline time
       _price = price; //setting price in wei per token
-      _limit = limit; //setting cap
+      //_limit = limit; //setting cap
       _wallet = wallet; //setting address the money will be sent to after time is done
+      //_start = openingtime;
+      //_end = closingtime;
   }
 
-  function() public isOver() payable {
-    buy(msg.sender);
-
+  function() external payable {
+    buyFor(msg.sender);
+  }
+  function hasClosed() public view returns (bool) {
+     //solium-disable-next-line security/no-block-members
+   return block.timestamp > _end;
   }
 
 
@@ -66,24 +59,23 @@ contract CrowdSale is ERCReceivingContract { //need ERC223 to notify when the co
       return _available;
   }
 
-  function buy() public payable {
-    buyFor(msg.sender);
 
-  }
-
-  function buyFor(address beneficiary) public available() valid(beneficiary, msg.value) payable {
-    require(beneficiary != address(0));
-    require(msg.value != 0);
-    uint amount = msg.value.div(_price);
-    _token.transfer(beneficiary,amount);
-    _limits[beneficiary] = _limits[beneficiary].add(amount);
-    _available = _available.sub(amount);
+  function buyFor(address beneficiary) public /*available() valid(beneficiary, msg.value)*/ payable {
+    //require(beneficiary != address(0));
+    //require(msg.value != 0);
+    uint256 weiamount = msg.value;
+    uint256 amount = weiamount.div(_price);
     _raised = _raised.add(amount);
-    emit Buy(beneficiary, amount);
+    _token.transfer(beneficiary,amount);
+    //_contributions[beneficiary] = _contributions[beneficiary].add(amount);
+    //_available = _available.sub(amount);
+    _forwardFunds();
+
+    emit TokenPurchase(msg.sender,beneficiary,weiamount,amount);
   }
 
-  function tokenFallback(address _from, uint _value, bytes _data) external isToken() {
-      _available += _value;
+  function _forwardFunds() internal {
+    _wallet.transfer(msg.value);
   }
 
 }
--- Test/contracts/TokenFromScratch3.sol
+++ Test/contracts/TokenFromScratch3.sol
@@ -2,18 +2,18 @@ pragma solidity ^0.4.18;
 
 import "./ERCReceivingContract.sol";
 import "./Token.sol";
+import "./SafeMath.sol";
 
 interface ERC20 {
-		function totalSupply() external constant returns (uint);
-    function balanceOf(address tokenOwner) external constant returns (uint balance);
-    function allowance(address tokenOwner, address spender) external constant returns (uint remaining);
-    function transfer(address _to, uint tokens) external returns (bool success);
-    function approve(address _spender, uint tokens) external returns (bool success);
-    function transferFrom(address _from, address _to, uint tokens) external returns (bool success);
-    event Transfer(address indexed from, address indexed to, uint value, bytes data);
-    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+		function totalSupply() public view returns (uint256);
+    function balanceOf(address tokenOwner) public view returns (uint256);
+    function allowance(address tokenOwner, address spender) public view returns (uint256);
+    function transfer(address _to, uint256 tokens) public returns (bool);
+    function approve(address _spender, uint256 tokens) public returns (bool);
+    function transferFrom(address _from, address _to, uint256 tokens) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value, bytes data);
+    event Approval(address indexed tokenOwner, address indexed spender, uint256 tokens);
 }
-
 
 library Addresses {
   function isContract(address _base) internal constant returns (bool) {
@@ -25,42 +25,46 @@ library Addresses {
   }
 }
 
-contract TokenFromScratch3 is Token("TokenFromScratch3", "TFS", 0, 10000), ERC20 {
+contract TokenFromScratch3 is Token("TokenFromScratch3", "TFS", 0, 50000000000000000000), ERC20 {
 
 	using Addresses for address;
+	using SafeMath for uint256;
 
-	constructor() TokenFromScratch3() public {
+	mapping(address=>uint256) public _balanceOf;
+  mapping(address => mapping(address=>uint256)) public _allowances;
+
+	function TokenFromScratch3() public {
 	    _balanceOf[msg.sender] = _totalsupply;
 
 	}
 
-	function totalSupply() external constant returns (uint) {
+	function totalSupply() public view returns (uint256) {
 		return _totalsupply;
 	}
 
-	function balanceOf(address _addr) external constant returns (uint) {
+	function balanceOf(address _addr) public view returns (uint256) {
 		return _balanceOf[_addr];
 	}
 
-	function transfer(address _to, uint tokens, bytes _data) internal returns (bool) {
+	function transfer(address _to, uint256 tokens, bytes _data) public returns (bool) {
 		if (tokens > 0 && tokens <= _balanceOf[msg.sender]) {
 			if(_to.isContract()){
 				ERCReceivingContract _contract = ERCReceivingContract(_to);
 				_contract.tokenFallback(msg.sender, tokens, _data);
 			}
-			_balanceOf[msg.sender] -= tokens;
-			_balanceOf[_to] += tokens;
+			_balanceOf[msg.sender] = _balanceOf[msg.sender].sub(tokens);
+			_balanceOf[_to] = _balanceOf[_to].add(tokens);
 			emit Transfer(msg.sender, _to, tokens, _data);
 			return true;
 		}
 		return false;
 	}
 
-	function transfer(address _to, uint tokens) external returns (bool){
+	function transfer(address _to, uint256 tokens) public returns (bool){
 		return transfer(_to, tokens, "");
 	}
 
-	function approve(address _spender, uint tokens) external returns (bool){
+	function approve(address _spender, uint tokens) public returns (bool){
 		if (tokens <= _balanceOf[msg.sender]) {
 			_allowances[msg.sender][_spender] = tokens;
 			emit Approval(msg.sender,_spender,tokens);
@@ -69,32 +73,47 @@ contract TokenFromScratch3 is Token("TokenFromScratch3", "TFS", 0, 10000), ERC20
 		return false;
 	}
 
-	function transferFrom(address _from, address _to, uint tokens, bytes _data) internal returns (bool) {
+	function transferFrom(address _from, address _to, uint tokens, bytes _data) public returns (bool) {
 		if (_balanceOf[_from] >= tokens &&
 			_allowances[_from][msg.sender] > 0 &&
 			_allowances[_from][msg.sender] >= tokens &&
 			tokens > 0){
-				_allowances[_from][msg.sender] -= tokens;
+				_allowances[_from][msg.sender] = _allowances[_from][msg.sender].sub(tokens);
 				if (_to.isContract()){
 					ERCReceivingContract _contract = ERCReceivingContract(_to);
 					_contract.tokenFallback(msg.sender, tokens, _data);
 				}
-			_balanceOf[_from] -= tokens;
-			_balanceOf[_to] += tokens;
+			_balanceOf[_from] = _balanceOf[_from].sub(tokens);
+			_balanceOf[_to] += _balanceOf[_to].add(tokens);
 			emit Transfer(_from, _to, tokens, _data);
 			return true;
 		}
 		return false;
 	}
-	function transferFrom(address _from, address _to, uint tokens) external returns (bool){
+	function transferFrom(address _from, address _to, uint256 tokens) public returns (bool){
 		return transferFrom(_from, _to, tokens, "");
 	}
 
-	function allowance(address tokenOwner, address spender) external constant returns (uint){
+	function allowance(address tokenOwner, address spender) public view returns (uint256){
 		if (_allowances[tokenOwner][spender] < _balanceOf[tokenOwner]) {
 			return _allowances[tokenOwner][spender];
 		}
 		return _balanceOf[tokenOwner];
 	}
+	function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
+    _allowances[msg.sender][_spender] = _allowances[msg.sender][_spender].add(_addedValue);
+    emit Approval(msg.sender, _spender, _allowances[msg.sender][_spender]);
+    return true;
+  }
+	function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
+    uint oldValue = _allowances[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      _allowances[msg.sender][_spender] = 0;
+    } else {
+      _allowances[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    emit Approval(msg.sender, _spender, _allowances[msg.sender][_spender]);
+    return true;
+  }
 
 }
